8a89017309a0bdaae0198af754b6200f6c1d3b95
HemanthRamapuram1995 <hemanth.smedia1995@gmail.com>
2022-07-30 20:08:42 +0530

    Added testcases
    Added testcases

---
+++ SwiftyLib/SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Hemanth Ramapuram on 30/07/22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
